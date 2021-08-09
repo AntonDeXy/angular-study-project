db571b1e6868c15288f57f893dcd034fc1e3589c
AntonDeXy <podolyananton@gmail.com>
2021-08-09 19:23:57 +0300

    module 9

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Pipe } from "@angular/core";
 import { IProduct } from './products';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -13,28 +14,7 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2
   showImage = false
   filteredProducts: IProduct[] = []
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    }
-  ]
+  products: IProduct[] = []
 
   private _listFilter: string = ''
 
@@ -48,6 +28,8 @@ export class ProductListComponent implements OnInit {
     this.filteredProducts = this.performFilter(value)
   }
 
+  constructor(private productService: ProductService) { }
+
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase()
     return this.products.filter(product => product.productName.toLocaleLowerCase().includes(filterBy))
@@ -58,7 +40,9 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.listFilter = 'cart'
+    this.products = this.productService.getProducts()
+    this.filteredProducts = this.products
+    this.listFilter = ''
   }
 
   onRatingClicked(message: string): void {
